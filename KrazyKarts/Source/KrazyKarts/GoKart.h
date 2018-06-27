063ceeef7e668018e21a4ee1d905678e568345c4
DESKTOP-LLDKI5Q\Karlito <astipanovic23@gmail.com>
2018-06-27 15:55:59 +0200

    74. Blocking Movement Without Physics

--- KrazyKarts/Source/KrazyKarts/GoKart.h
+++ KrazyKarts/Source/KrazyKarts/GoKart.h
@@ -23,6 +23,8 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	void UpdateLocationFromVelocity(float DeltaTime);
+
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
